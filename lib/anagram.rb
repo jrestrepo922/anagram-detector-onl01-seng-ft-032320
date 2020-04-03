de35d868277a122f25529e1bf5f5bcc34211f33e
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-03 18:35:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,9 +7,10 @@ class Anagram
   end 
   
   def match(array_of_words)
-    anagram_word = array_of_words.find { |word| word.split("").sort == word_provided.split("").sort }
+    anagram_word = array_of_words.find { |word| word.split("").sort == @word.split("").sort }
     
     anagram_array = []
     anagram_array << anagram_word
   end 
+  
 end 
